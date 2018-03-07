8675d41dca54bb5e55e28469f7813154e9c055c8
HASHIMOTO Wataru <watr@users.noreply.github.com>
2018-03-07 09:58:52 +0900

    Add savep

---
+++ savep.rb
@@ -0,0 +1,12 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+ class Savep < Formula
+  desc "Just shows save panel GUI to select directory/file, which may fulfills interactive selection in script."
+  homepage "https://github.com/watr/savep"
+  url "https://github.com/watr/savep/releases/download/v0.0.1/savep.zip"
+  sha256 "9b0c6311199839a570ad61f3a4d52b0af2f80cdd31b08701ace47bcfe073b3de"
+   def install
+     bin.install 'savep'
+  end
+end
