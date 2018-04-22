b6f1fd0a5c19d30eaf0dee3425671acd9968946c
HASHIMOTO Wataru <watr@users.noreply.github.com>
2018-04-22 12:58:39 +0900

    mbeacon v0.0.2

--- mbeacon.rb
+++ mbeacon.rb
@@ -1,8 +1,8 @@
 class Mbeacon < Formula
   desc "Turn your mac into an iBeacon."
   homepage ""
-  url "https://github.com/watr/mbeacon/releases/download/v0.0.1/mbeacon.zip"
-  sha256 "722c8b29900c4521ff4a27eef3c2e94668154650ea0b4e51b53be04c3228ed56"
+  url "https://github.com/watr/mbeacon/releases/download/v0.0.2/mbeacon.zip"
+  sha256 "89bb5e5ab6fdf53f0f3775c1d62b2da0ecbe28f92504322fd66bff1389eaa33e"
 
   def install
     bin.install 'mbeacon'
