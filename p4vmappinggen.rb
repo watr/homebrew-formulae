fde8938e6d0ba716d95ec7bf82c2ac0846d9a38a
Hashimoto Wataru <watr@users.noreply.github.com>
2018-11-21 14:29:31 +0900

    Update p4vmappinggen.rb

--- p4vmappinggen.rb
+++ p4vmappinggen.rb
@@ -1,8 +1,8 @@
 class P4vmappinggen < Formula
   desc "Generate Perforce workspace mapping from JSON."
-  homepage ""
-  url "https://github.com/watr/p4vmappinggen/releases/download/v0.1/p4vmappinggen.rb"
-  sha256 "e10440c7d7fbe579fb322586d58db20d6d07af248266f7d3938b5f4296b283d2"
+  homepage "https://github.com/watr/p4vmappinggen"
+  url "https://github.com/watr/p4vmappinggen/releases/download/v0.2/p4vmappinggen.rb"
+  sha256 "da426a2b0ead94f8f3ce78cdbee1e5fbffc6f91c530e18a541f5fe5bba5c5b1f"
 
   def install
     bin.install "p4vmappinggen.rb" => "p4vmappinggen"
